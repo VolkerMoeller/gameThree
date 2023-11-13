f63a385df3305c77dcd9678f3e27cf584dd4df6d
VolkerMoeller <126388919+VolkerMoeller@users.noreply.github.com>
2023-11-13 10:50:49 +0100

    images loaded

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,8 +3,21 @@ class Chicken extends MoveableObject {
     width = Math.floor(248 / 4);
     height = Math.floor(243 / 4);
 
+    IMAGES_WALK = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+    ];
+
+    IMAGES_DEAD = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
+
+
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.x = Math.random() * 550 + 150;
+        this.loadImages(this.IMAGES_WALK);
+        this.loadImages(this.IMAGES_DEAD);
+        this.x = Math.floor(Math.random() * 550) + 150;
     }
 }
--- models/chickenSmall.class.js
+++ models/chickenSmall.class.js
@@ -3,8 +3,20 @@ class ChickenSmall extends MoveableObject {
     width = Math.floor(236 / 5);
     height = Math.floor(210 / 5);
 
+    IMAGES_WALK = [
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+
+    IMAGES_DEAD = [
+        'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
+
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
-        this.x = Math.random() * 550 + 150;
+        this.loadImages(this.IMAGES_WALK);
+        this.loadImages(this.IMAGES_DEAD);
+        this.x = Math.floor(Math.random() * 550) + 150;
     }
 }
